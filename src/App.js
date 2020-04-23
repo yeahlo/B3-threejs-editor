5a425f6d167e8a70ea3e7988907deab244918767
bcandas <candasbertrand@gmail.com>
2020-04-23 13:28:24 +0200

    clean renderer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import React, {useRef, useContext} from 'react';
-=======
 import React, {useContext, useRef} from 'react';
->>>>>>> 315cc39c51a8e1ca8e48ca7c5e67249f76f7f627
 import './App.scss';
 import {Switch, Route, Link} from "react-router-dom";
 import routes from "./constants/routes";
@@ -12,16 +8,6 @@ import Textures from "./options/textures";
 import Shapes from "./options/shapes";
 import Renderer from "./renderer";
 import Gadget from "./options/gadgets";
-<<<<<<< HEAD
-import { Store, updateColor } from './store';
-
-const App = props => {
-    const {dispatch, store} = useContext(Store)
-    return (
-        <div className="App">
-
-            <button onClick={() => {dispatch(updateColor(Math.random() * 0xFFFFFF))}}>
-=======
 import {Store, updateColor} from "./store";
 
 const App = props => {
@@ -30,12 +16,6 @@ const App = props => {
 
     return (
         <div className="App">
-
-            <button onClick={() => dispatch(updateColor(Math.random() * 0XFFFFFF))}>
->>>>>>> 315cc39c51a8e1ca8e48ca7c5e67249f76f7f627
-                color
-            </button>
-
             <Switch>
                 <Route exact path={routes.home.options.colors.all} component={Colors}/>
                 <Route exact path={routes.home.options.shapes.all} component={Shapes}/>
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import React, { useRef, useEffect, useContext } from 'react';
-import { Store } from '../store';
-import {WebGLRenderer, Scene, PerspectiveCamera, BoxGeometry, Mesh, MeshLambertMaterial, HemisphereLight, StaticReadUsage, Color, TextureLoader} from 'three';
-
-const Renderer = props => {
-    const {dispatch, state} = useContext(Store)
-
-    const renderer = useRef(new WebGLRenderer());
-    const scene = useRef(new Scene());
-    const camera = useRef(new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000))
-    const cube = useRef(null);
-
-=======
 import React from 'react';
 import {useEffect, useRef, useContext} from "react";
 import {
@@ -34,54 +20,10 @@ const Renderer = props => {
     const scene = useRef(null);
     const camera = useRef(null);
     const cube = useRef(null);
->>>>>>> 315cc39c51a8e1ca8e48ca7c5e67249f76f7f627
 
     const container = useRef(null);
 
     useEffect(() => {
-<<<<<<< HEAD
-        if(state.color){
-            cube.current.material.color = new Color().setHex(state.color)
-        }
-    })
-
-    useEffect(() => {
-        console.log('renderer', renderer)
-
-        const update = () => {
-            requestAnimationFrame(update)
-
-            cube.current.rotation.x += 0.01;
-            cube.current.rotation.y += 0.01;
-
-            const txture = loader.load('/img/' + state.texture + '.jpg')
-            cube.current.map = txture
-
-            renderer.current.render(scene.current, camera.current)
-        }
-
-        renderer.current.setSize(500, 500);
-        camera.current.position.z = 3
-        container.current.appendChild(renderer.current.domElement)
-
-        const hemiLight = new HemisphereLight(0xfffffff, 0xffffff, 0.8)
-        hemiLight.color.setHSL(0.6, 1, 0.6)
-        hemiLight.groundColor.setHSL(0.095, 1, 0.75);
-        hemiLight.position.set(0, 50, 0)
-        scene.current.add(hemiLight);
-
-        const geometry = new BoxGeometry();
-
-        const loader = new TextureLoader()
-        const txture = loader.load('/img/' + state.texture + '.jpg')
-
-        const material = new MeshLambertMaterial({color: 0x00FF00, map: txture});
-        cube.current = new Mesh(geometry, material);
-        scene.current.add(cube.current)
-
-        update()
-    }, [renderer])
-=======
         if(state.color) {
             cube.current.material.color = new Color().setRGB(
                 state.color.r/255,
@@ -146,7 +88,6 @@ const Renderer = props => {
         }
 
     }, [renderer]);
->>>>>>> 315cc39c51a8e1ca8e48ca7c5e67249f76f7f627
 
     return (
         <div className="Renderer" ref={container}>
